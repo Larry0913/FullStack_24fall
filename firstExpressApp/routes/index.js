a6a41e5aaaa408763e26e6dda9599d16f30d8690
Larry0913 <lilingjie0913@gmail.com>
2024-10-26 00:05:28 -0700

    finish basic assignment2 q2

--- firstExpressApp/routes/index.js
+++ firstExpressApp/routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
-var router = express.Router();
+var router = express();
+
+let data = [];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { 
+    title: 'Welcome to larry Express assignment!' 
+  });
 });
+
+router.get('/tweet', (req, res, next) => {
+  res.render('index', { 
+    title: 'Tweet', 
+    contents: 'Show tweets'
+  });
+});
+
+router.post('/', (req, res, next) => {
+  console.log(req.body);
+  data.push(req.body);
+  res.render('index', { 
+    id: data[0].id,
+    title: data[0].title, 
+    content: data[0].content
+  });
+});
+
+router.get('/tweet/:id', (req, res, next) => {
+  res.render('index', { 
+    title: 'tweet', 
+    contents: `delete a tweet id ${req.params.id}`
+  });
+});
+
 
 module.exports = router;
